b50e4d44237f0d173e8cd6dece278c01c28015df
Justice Marks <her@shedaddy.com>
2019-06-04 02:55:48 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,7 +2,7 @@ def meal_choice(protein="meat",veg1,veg2)
   
   puts "What a nutritious meal!"
   
-  result = "A plate of #{protein} with #{veg1} and #{veg2}"
+  result = "A plate of #{protein} with #{veg1} and #{veg2}."
   
   puts result
   
